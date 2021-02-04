4d5c049277c643b0d47a2080a7ea54102f49fbd0
Ben Feder <benfeder@Ben.localdomain>
2021-02-04 17:00:05 -0500

    Passes encode basic test

--- rot13.py
+++ rot13.py
@@ -6,7 +6,7 @@ Kenzie Assignment: rot13
 
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Benjamin Feder"
 
 import sys
 from string import ascii_lowercase, ascii_uppercase
@@ -17,7 +17,19 @@ def rotate(message):
     Returns the encoded or decoded message.
     """
     rot_message = ''
-    # Your code here
+    for letter in range(len(message)):
+
+        change = 13
+
+        value = ord(message[letter])
+
+        if value + change > 122:
+            change -= (122 - value)
+            change = change % 26
+            rot_message += chr(96 + change)
+
+        else:
+            rot_message += chr(value + change)
 
     return rot_message
 
